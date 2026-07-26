95ae9619d159743971907ad96efb7c7cde4fb4c4
Dev Bolton <jabolton939@gmail.com>
2026-07-26 12:39:41 -0400

    Started pybind for tensor

---
+++ framework/csrc/pybind/tensor_bind.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "../tensor/tensor.hpp"
+#include "../tensor/dtype.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(tensor, m) {
+    py::class_<Tensor<float>>(m, "Tensor")
+        .def(py::init<std::vector<size_t>, const float&>(), py::arg("shape"), py::arg("initial_val") = 0.0f)
+        .def(py::init<std::vector<float>>(), py::arg("list_data"))
+        .def("data", static_cast<std::vector<float>& (Tensor<float>::*)()>(&Tensor<float>::data))
+        .def("size", &Tensor<float>::size)
+        .def("shape", &Tensor<float>::shape)
+        .def("reshape", &Tensor<float>::reshape, py::arg("new_shape"))
+        .def_static("transpose", &Tensor<float>::transpose)
+        .def_static("multiply", &Tensor<float>::multiply)
+        .def_static("subtract", &Tensor<float>::subtract)
+        .def("__str__", &Tensor<float>::toString);
+}
