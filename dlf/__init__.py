cd8166c52d71e8747097aa6df3942863236463d3
Dev Bolton <jabolton939@gmail.com>
2026-07-27 21:51:54 -0400

    Added model to dlf init

--- dlf/__init__.py
+++ dlf/__init__.py
@@ -1,4 +1,4 @@
 from ._dlf import *
-from ._dlf import Tensor, Linear, MSEloss
+from ._dlf import Tensor, Linear, MSEloss, Model
 
-__all__ = ["Tensor", "Linear", "MSEloss"]
+__all__ = ["Tensor", "Linear", "MSEloss", "Model"]
