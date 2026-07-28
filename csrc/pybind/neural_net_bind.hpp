9dbbe8ff7b02a8ac024c5ca4381496836377ce22
Dev Bolton <jabolton939@gmail.com>
2026-07-27 21:45:40 -0400

    Added bind for model

--- csrc/pybind/neural_net_bind.hpp
+++ csrc/pybind/neural_net_bind.hpp
@@ -1,8 +1,23 @@
 #include <pybind11/pybind11.h>
 #include "../neural_net/layers.hpp"
+#include "../neural_net/model.hpp"
 #include "../tensor/tensor.hpp"
 
 namespace py = pybind11;
+
+class PyModel : public Model<float> {
+    public:
+        using Model<float>::Model;
+
+        Tensor<float> forward(const Tensor<float>& x) override {
+            PYBIND11_OVERRIDE_PURE(
+                Tensor<float>,
+                Model<float>,
+                forward,
+                x
+            );
+        }
+};
 
 void bind_neural_net(py::module_& m) {
     py::class_<Layer<float>>(m, "Layer");
@@ -11,5 +26,18 @@ void bind_neural_net(py::module_& m) {
         .def(py::init<size_t, size_t, bool>(), py::arg("inFeats"), py::arg("outFeats"), py::arg("bias") = true)
         .def("forward", &Linear<float>::forward, py::arg("x"))
         .def("backward", &Linear<float>::backward, py::arg("lGrad"));
+
+    py::class_<Model<float>, PyModel, std::shared_ptr<Model<float>>>(m, "Model")
+        .def(py::init<>())
+        .def("train", &Model<float>::train)
+        .def("eval", &Model<float>::eval)
+        .def("is_training", &Model<float>::isTraining)
+        .def("add_layer", &Model<float>::addLayer, py::arg("layer"), py::keep_alive<1,2>())
+        .def("forward", &Model<float>::forward, py::arg("x"))
+        .def("backward", &Model<float>::backward, py::arg("l_grad"))
+        .def("step", &Model<float>::step, py::arg("x"), py::arg("y_true"));
+
+
+
 
 }
