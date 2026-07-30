7a39aafa49267a9941b02dcf24f3c8f6b499c30d
Dev Bolton <jabolton939@gmail.com>
2026-07-30 09:27:36 -0400

    Fixed CUDA build errors

--- csrc/cuda/accelerator.hpp
+++ csrc/cuda/accelerator.hpp
@@ -19,15 +19,20 @@ struct DeviceInfo{
 
 class Accelerator {
     public:
+        
         static bool available(){
 #ifdef DLF_CUDA
-            int count = 0;
-            cudaError_t err = cudaGetDeviceCount(&count);
-            return (err == cudaSuccess) && (count > 0);
-#else
-            return false;
+    int count = 0;
+    cudaError_t err = cudaGetDeviceCount(&count);
+    printf("cudaGetDeviceCount: err=%d (%s), count=%d\n", (int)err, cudaGetErrorString(err), count);
+    fflush(stdout);
+    return (err == cudaSuccess) && (count > 0);
+#else   
+    printf("DLF_CUDA not defined at compile time\n");
+    fflush(stdout);
+    return false;
 #endif
-        }
+}
 
         static std::vector<DeviceInfo> listDevices(){
             std::vector<DeviceInfo> devices;
@@ -36,7 +41,7 @@ class Accelerator {
             if (cudaGetDeviceCount(&count) != cudaSuccess) return devices;
             for (int i = 0; i < count; ++i){
                 cudaDeviceProp prop;
-                if (cudaDeviceProperies(&prop, i) == cudaSuccess) {
+                if (cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
                     devices.push_back({i, std::string(prop.name), prop.totalGlobalMem});
                 }
             }
