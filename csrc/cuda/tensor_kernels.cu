ef8296d7231bcf7880a2f2e47104694b20706e9d
Dev Bolton <jabolton939@gmail.com>
2026-08-04 15:37:41 -0400

    Created TSub CUDA kernel

--- csrc/cuda/tensor_kernels.cu
+++ csrc/cuda/tensor_kernels.cu
@@ -23,6 +23,13 @@ __global__ void taddKernel(const float* A, const float* B, float* C, size_t n){
     }
 }
 
+__global__ void tsubKernel(const float* A, const float* B, float* C, size_t n){
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if(idx<n){
+        C[idx] = A[idx] - B[idx];
+    }
+}
+
 void launchTMul(const float* A, const float* B, float* C,size_t M, size_t K, size_t N){
     dim3 block(16, 16);
     dim3 grid((N + 15) / 16, (M + 15) / 16);
@@ -38,4 +45,14 @@ void launchTAdd(const float* A, const float* B, float* C, size_t n){
     if (err != cudaSuccess) throw std::runtime_error(std::string("TAddKernel launch failed") + cudaGetErrorString(err));
     cudaDeviceSynchronize();
     if (err != cudaSuccess) throw std::runtime_error("TAddKernel execution failed");
+}
+
+void launchTSub(const float* A, const float* B, float* C, size_t n){
+    unsigned int threads = 256;
+    unsigned int blocks = (unsigned int)((n + threads - 1) / threads);
+    taddKernel<<<blocks, threads>>>(A, B, C, n);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) throw std::runtime_error(std::string("TSubKernel launch failed") + cudaGetErrorString(err));
+    cudaDeviceSynchronize();
+    if (err != cudaSuccess) throw std::runtime_error("TSubKernel execution failed");
 }
